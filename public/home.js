2e9cdb9c6e291684bc546ac22ab4cb26c8d7bd0d
StefanMiloj <84393700+StefanMiloj@users.noreply.github.com>
2022-02-28 12:02:33 -0500

    Add files via upload

---
+++ public/home.js
@@ -0,0 +1,10 @@
+function Home(){
+  return (
+    <Card
+      txtcolor="black"
+      title="Welcome to Little Man Bank"
+      text="...Where we are happy to serve any individual, no matter the size"
+      body={(<img src="andrew.jpg" className="img-fluid" alt="Responsive image"/>)}
+    />
+  );  
+}
